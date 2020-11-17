f3118554345fdc147d984bc49c920d9dbb193acc
Matthew <mblecke@gmail.com>
2020-11-17 10:43:23 -0600

    fixed coding errors

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,15 +2,26 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
-import Modal from './modules/Modal'
 
-let mobileMenu = new MobileMenu();
-let stickyHeader = new StickyHeader();
+new StickyHeader()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new MobileMenu();
+let modal
 
-new Modal()
-
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if (typeof modal == "undefined") {
+            import('./modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log("There was a problem."))
+        } else {
+            modal.openTheModal()
+        }   
+    })
+})
 
 if (module.hot) {
     module.hot.accept();
