3428dfc4723723d5152e1d1b04036b05922c2f71
Matthew <mblecke@gmail.com>
2020-11-19 09:32:31 -0600

    set up build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const postCSSPlugins = [
     require('postcss-import'),
     require('postcss-mixins'),
@@ -7,13 +11,29 @@ const postCSSPlugins = [
     require('postcss-hexrgba'),
     require('autoprefixer')
 ]
-module.exports = {
+
+letcssConfig = {
+    test: /\.css$/i,
+    use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+}
+
+let config = {
     entry: './app/assets/scripts/App.js',
-    output: {
+    plugins: [new HtmlWebpackPlugin({filename: 'index.html', template: './app/index.html'})],
+    module: {
+        rules: [
+            cssConfig
+        ]
+    },
+}
+
+if (currentTask == 'dev') {
+    cssConfig.use.unshift('style-loader'),
+    config.output = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
-    }, 
-    devServer: {
+    },
+    config.devServer = {
         before: function(app, server){
             server._watch('./app/**/*.html')
         },
@@ -22,13 +42,22 @@ module.exports = {
         port: 3000,
         host: '0.0.0.0'
     },
-    mode: "development",
-       module: {
-           rules: [
-               {
-                   test: /\.css$/i,
-                   use: ['style-loader','css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
-               }
-           ]
-       }
+    config.mode = "development"
 }
+
+if (currentTask == 'build') {
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+    postCSSPlugins.push(require('cssnano'))
+    config.output = {
+        filename: '[].[chunkhash].js',
+        chunkFilename: '[].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+    config.mode = "production"
+    config.optimization = {
+        splitChunks: {chunks: 'all'}
+    }
+    config.plugins.push(new CleanWebpackPlugin(), new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'})) 
+}
+
+module.exports = config
